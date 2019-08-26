459b60624081061bc36b9eb8eaa26b564c019310
misspia <pia_leung@hotmail.com>
2019-08-26 17:49:47 -0400

    Define evolution type with trigger Ids

--- src/models/getEvolutionByChainId.js
+++ src/models/getEvolutionByChainId.js
@@ -14,14 +14,31 @@ function getEvolutionNodes(chainId) {
       return chainNodes;
     }
     const {
-      id,
-      identifier: name,
       evolves_from_species_id: evolvesFromId,
     } = pokemon;
     chainNodes.push({
-      id,
-      name,
+      id: pokemon.id,
+      name: pokemon.identifier,
       evolvesFromId: evolvesFromId === '' ? null : evolvesFromId,
+      evolutionTriggerId: pokemon.evolution_trigger_id,
+      triggerItemId: pokemon.trigger_item_id,
+      minimumLevel: pokemon.minimum_level,
+      genderId: pokemon.gender_id,
+      locationId: pokemon.location_id,
+      heldItemId: pokemon.held_item_id,
+      timeOfDay: pokemon.time_of_day,
+      knownMoveId: pokemon.known_move_id,
+      knownMoveTypeId: pokemon.known_move_type_id,
+      minimumHappiness: pokemon.minimum_happiness,
+      minimumBeauty: pokemon.minimum_beauty,
+      minimumAffection: pokemon.minimum_affection,
+      relativePhysicalStats: pokemon.relative_physical_stats,
+      partySpeciesId: pokemon.party_species_id,
+      partyTypeId: pokemon.party_type_id,
+      tradeSpeciesId: pokemon.trade_species_id,
+      needsOverworldRain: pokemon.needs_overworld_rain === 1,
+      turnUpsideDown: pokemon.turn_upside_down === 1,
+
     })
     return chainNodes;
   }, []);
--- src/schema/evolution.schema.js
+++ src/schema/evolution.schema.js
@@ -10,6 +10,23 @@ const typeDefs = gql`
     id: Int
     name: String
     evolvesFromId: Int
+    evolutionTriggerId: Int
+    triggerItemId: Int
+    minimumLevel: Int
+    genderId: Int
+    locationId: Int
+    heldItemId: Int
+    timeOfDay: String
+    knownMoveId: Int
+    knowMoveTypeId: Int
+    mimimumHappiness: Int
+    minimumBeauty: Int
+    minimumAffection: Int
+    relativePhysicalStats: Int
+    partySpeciesId: Int
+    tradeSpeciesId: Int
+    needsOverworldRain: Boolean
+    turnUpsideDown: Boolean
   }
 `;
 
