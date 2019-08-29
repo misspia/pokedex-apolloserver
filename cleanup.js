452d909a10438c83dc5f9a5d8c93df58fb0b161a
misspia <pia_leung@hotmail.com>
2019-08-29 16:24:35 -0400

    output data to new folder

--- cleanup.js
+++ cleanup.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
+const INPUT_DIRECTORY = './src/rawdata';
+const OUTPUT_DIRECTORY = './src/data';
 
-let rawDataFilepaths = getFilepaths('src/rawdata')
-  .concat(getFilepaths('src/rawdata/pokemon'));
+
+const rawDataFilepaths = getFilepaths(INPUT_DIRECTORY)
+  .concat(getFilepaths(`${INPUT_DIRECTORY}/pokemon`));
 
 function getFilepaths(directory) {
   const isJsonFile = (filename => (/json/g).test(filename));
@@ -14,3 +17,17 @@ function getFilepaths(directory) {
     return filepaths
   }, []);
 }
+
+rawDataFilepaths.forEach(filepath => {
+  const data = JSON.parse(fs.readFileSync(filepath, 'utf-8'));
+  const filename = filepath.replace(INPUT_DIRECTORY, '');
+
+  const newPath = `${OUTPUT_DIRECTORY}${filename}`;
+  fs.writeFile(newPath, JSON.stringify(data, null, 2), 'utf8', (err) => {
+    if (err) {
+      return console.log('Unable to write to ', newPath);
+    }
+    console.log('Wrote file to ', newPath);
+
+  });
+});
